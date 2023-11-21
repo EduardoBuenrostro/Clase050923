6d0511208a06ee783e22231752fbfed704bc4eae
EduardoBuenrostro <150473216+EduardoBuenrostro@users.noreply.github.com>
2023-11-21 12:13:06 -0600

    Add files via upload

---
+++ Clase050923/app.js
@@ -0,0 +1,36 @@
+let x=10;
+let y=2;
+let resultado=x+y;
+console.log(resultado);
+
+var uno="Hola";
+var dos="Mundo";
+var joined =uno+dos;
+console.log(joined);
+
+var numeroUno=200;
+var numeroDos=100;
+console.log(numeroUno+numeroDos);
+console.log(typeof numeroUno);
+
+var nombre=prompt("Ingresa tu nombre: ");
+console.log(typeof nombre);
+console.log(nombre);
+
+let numero1=prompt("Ingresa un número: ");
+let numero2=prompt("Ingresa un segundo número: ");
+
+let result = parseInt(numero1)+parseInt(numero2);
+console.log(result);
+
+let numero3=prompt("Ingresa un número: ");
+let numero4=prompt("Ingresa un segundo número: ");
+
+let resta = parseInt(numero3)+parseInt(numero4);
+console.log(resta);
+
+let numero5=prompt("Ingresa un número: ");
+let numero6=prompt("Ingresa un segundo número: ");
+
+let multi = parseInt(numero5)+parseInt(numero6);
+console.log(multi);
